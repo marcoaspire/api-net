fcbef3e0739a43245be70f1fdd76a79414523664
Marco <ocrampac@gmail.com>
2025-01-11 10:08:28 -0600

    api added

---
+++ api/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+
+        public DbSet<Comment> Comments {get; set; }
+    }
+}
---
+++ api/Program.cs
@@ -0,0 +1,47 @@
+// using EntityFrameworkCore.MySQL.Data;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using api.Repository;
+
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
+var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 4, 3))));
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//
+app.MapControllers();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
